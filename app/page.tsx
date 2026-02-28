aa76424d2d3ddedd59cd26c7f964fca65e5d0450
hakemali120 <hakemali120@gmail.com>
2026-02-28 10:09:54 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import RootLayout from "./layout"
+import RootLayout from "@/app/layout" // استخدم @/app للوصول المباشر
 
 export default function SyntheticV0PageForDeployment() {
   return <RootLayout />
